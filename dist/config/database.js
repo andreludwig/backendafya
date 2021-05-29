7aa623f8a710e16d460094d8546c8cad9ded3127
andreludwig <andre@roadseg.com.br>
2021-05-29 16:55:13 -0300

    first commit

---
+++ dist/config/database.js
@@ -0,0 +1,12 @@
+"use strict";module.exports = {
+    dialect: 'postgres',
+    host: 'batyr.db.elephantsql.com',
+    username: 'fncdpfcr',
+    password: 'ket544P9jTfNdylGGUG40qtv6t_tw-Hd',
+    database: 'fncdpfcr',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    }
+}
---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+
+import UserController from './app/controller/UserController'
+import SessionController from './app/controller/SessionController'
+
+import authMiddleware from './app/middlewares/auth'
+
+const routes = new Router();
+
+// Criar usuário
+routes.post('/users', UserController.store)
+
+// Logar
+routes.post('/session', SessionController.store)
+
+// Autenticação
+routes.use(authMiddleware)
+
+// Logar
+routes.post('/dash', authMiddleware, UserController.index)
+
+export default routes;
